bb6c18f813f5690eaa3092e20ac291573c62e40f
Seth R Johnson <johnsonsr@ornl.gov>
2021-04-16 06:53:11 -0400

    Update eplusgg kernel for upstream changes + single precision
    I manually changed real_type to "float".

--- celer-eplusgg-kernel/kernel.cu
+++ celer-eplusgg-kernel/kernel.cu
@@ -1,35 +1,23 @@
+#include "base/Assert.hh"
+#include "base/KernelParamCalculator.cuda.hh"
+#include "random/RngEngine.hh"
+#include "physics/base/ModelInterface.hh"
 #include "physics/base/ParticleTrackView.hh"
-#include "physics/base/SecondaryAllocatorPointers.hh"
-#include "physics/base/SecondaryAllocatorView.hh"
-#include "physics/em/EPlusGGInteractor.hh"
-#include "random/cuda/RngEngine.cuh"
+#include "physics/base/PhysicsTrackView.hh"
+#include "base/StackAllocator.hh"
+#include "physics/em/detail/EPlusGGInteractor.hh"
 
 using namespace celeritas;
+using namespace celeritas::detail;
 
-struct Params {
-  ParticleParamsPointers particle;
-  EPlusGGInteractorPointers interactor;
-};
+__global__ void test_kernel(const EPlusGGPointers epgg,
+                            const ModelInteractPointers model) {
+  auto tid = celeritas::KernelParamCalculator::thread_id();
 
-struct State {
-  ParticleStatePointers particle;
-  RngStatePointers rng;
-  span<const Real3> direction;
-  span<Interaction> result;
-};
-
-__global__ void test_kernel(Params const params, State const states,
-                            SecondaryAllocatorPointers const secondaries) {
-  int tid = blockIdx.x * blockDim.x + threadIdx.x;
-
-  SecondaryAllocatorView allocate_secondaries(secondaries);
-  ParticleTrackView particle(params.particle, states.particle, ThreadId(tid));
-  RngEngine rng(states.rng, ThreadId(tid));
-
-  EPlusGGInteractor interact(params.interactor, particle, states.direction[tid],
+  StackAllocator<Secondary> allocate_secondaries(model.secondaries);
+  ParticleTrackView particle(model.params.particle, model.states.particle, tid);
+  EPlusGGInteractor interact(epgg, particle, model.states.direction[tid.get()],
                              allocate_secondaries);
-
-  Interaction result = interact(rng);
-
-  states.result[tid] = result;
+  RngEngine rng(model.states.rng, tid);
+  model.result[tid.get()] = interact(rng);
 }

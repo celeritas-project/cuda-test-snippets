84c6f601cb017c55afcee49585c9dca1ad1f511f
Seth R Johnson <johnsonsr@ornl.gov>
2021-10-26 08:07:52 -0400

    Store RNG locally

--- celer-rng-uniform/canonical.s
+++ celer-rng-uniform/canonical.s
@@ -16,207 +16,191 @@ compile_size = 64bit
 
         /*0020*/                   S2R R0, SR_CTAID.X ;
 
-        /*0030*/                   IMAD.MOV.U32 R17, RZ, RZ, 0x30 ;
+        /*0030*/                   IMAD.MOV.U32 R15, RZ, RZ, 0x30 ;
 
         /*0040*/                   S2R R3, SR_TID.X ;
 
         /*0050*/                   IMAD R0, R0, c[0x0][0x0], R3 ;
 
-        /*0060*/                   IMAD.WIDE.U32 R16, R0, R17, c[0x0][0x160] ;
+        /*0060*/                   IMAD.WIDE.U32 R14, R0, R15, c[0x0][0x160] ;
 
-        /*0070*/                   LDG.E.64.SYS R2, [R16] ;
+        /*0070*/                   LDG.E.64.SYS R10, [R14] ;
 
-        /*0080*/                   LDG.E.64.SYS R14, [R16+0x8] ;
+        /*0080*/                   LDG.E.64.SYS R2, [R14+0x8] ;
 
-        /*0090*/                   LDG.E.64.SYS R8, [R16+0x10] ;
+        /*0090*/                   LDG.E.64.SYS R4, [R14+0x10] ;
 
-        /*00a0*/                   IMAD.MOV.U32 R18, RZ, RZ, 0x0 ;
+        /*00a0*/                   LDG.E.64.SYS R8, [R14+0x18] ;
 
-        /*00b0*/                   IMAD.MOV.U32 R19, RZ, RZ, 0x3ca00000 ;
+        /*00b0*/                   LDG.E.64.SYS R12, [R14+0x28] ;
 
-        /*00c0*/                   IMAD.MOV.U32 R11, RZ, RZ, 0x8 ;
+        /*00c0*/                   LDG.E.SYS R21, [R14+0x20] ;
 
-        /*00d0*/                   IMAD R10, R0, 0x3, RZ ;
+        /*00d0*/                   IMAD.MOV.U32 R28, RZ, RZ, 0x0 ;
 
-        /*00e0*/                   IMAD.WIDE.U32 R10, R10, R11, c[0x0][0x170] ;
+        /*00e0*/                   IMAD.MOV.U32 R29, RZ, RZ, 0x40150000 ;
 
-        /*00f0*/                   SHF.R.U32.HI R4, RZ, 0x2, R3 ;
+        /*00f0*/                   IMAD.MOV.U32 R20, RZ, RZ, 0x8 ;
 
-        /*0100*/                   LOP3.LUT R4, R4, R3, RZ, 0x3c, !PT ;
+        /*0100*/                   IMAD R0, R0, 0x3, RZ ;
 
-        /*0110*/                   STG.E.SYS [R16+0x4], R15 ;
+        /*0110*/                   SHF.R.U32.HI R6, RZ, 0x2, R11 ;
 
-        /*0120*/                   SHF.R.U32.HI R5, RZ, 0x2, R14 ;
+        /*0120*/                   LOP3.LUT R6, R6, R11, RZ, 0x3c, !PT ;
 
-        /*0130*/                   IMAD.SHL.U32 R6, R9, 0x10, RZ ;
+        /*0130*/                   SHF.R.U32.HI R17, RZ, 0x2, R2 ;
 
-        /*0140*/                   STG.E.64.SYS [R16+0x8], R8 ;
+        /*0140*/                   IMAD.SHL.U32 R7, R5, 0x10, RZ ;
 
-        /*0150*/                   IMAD.SHL.U32 R3, R4, 0x2, RZ ;
+        /*0150*/                   LOP3.LUT R2, R17, R2, RZ, 0x3c, !PT ;
 
-        /*0160*/                   LOP3.LUT R5, R5, R14, RZ, 0x3c, !PT ;
+        /*0160*/                   IMAD.SHL.U32 R11, R6.reuse, 0x2, RZ ;
 
-        /*0170*/                   IADD3 R14, R2, 0xb0f8a, RZ ;
+        /*0170*/                   LOP3.LUT R16, R6, R7, R5, 0x96, !PT ;
 
-        /*0180*/                   LOP3.LUT R3, R9, R3, R4, 0x96, !PT ;
+        /*0180*/                   IMAD.SHL.U32 R6, R2, 0x2, RZ ;
 
-        /*0190*/                   IMAD.SHL.U32 R4, R5, 0x2, RZ ;
+        /*0190*/                   LOP3.LUT R11, R16, R11, RZ, 0x3c, !PT ;
 
-        /*01a0*/                   LOP3.LUT R6, R3, R6, RZ, 0x3c, !PT ;
+        /*01a0*/                   SHF.R.U32.HI R16, RZ, 0x2, R3 ;
 
-        /*01b0*/                   STG.E.SYS [R16], R14 ;
+        /*01b0*/                   LOP3.LUT R7, R11.reuse, R6, R2, 0x96, !PT ;
 
-        /*01c0*/                   LOP3.LUT R4, R6.reuse, R4, R5, 0x96, !PT ;
+        /*01c0*/                   IMAD.SHL.U32 R2, R11, 0x10, RZ ;
 
-        /*01d0*/                   IMAD.SHL.U32 R7, R6, 0x10, RZ ;
+        /*01d0*/                   LOP3.LUT R16, R16, R3, RZ, 0x3c, !PT ;
 
-        /*01e0*/                   IADD3 R3, R2, 0x587c5, R6 ;
+        /*01e0*/                   LOP3.LUT R3, R7, R2, RZ, 0x3c, !PT ;
 
-        /*01f0*/                   LOP3.LUT R7, R4, R7, RZ, 0x3c, !PT ;
+        /*01f0*/                   IMAD.SHL.U32 R2, R16, 0x2, RZ ;
 
-        /*0200*/                   IMAD.IADD R4, R7, 0x1, R14 ;
+        /*0200*/                   SHF.R.U32.HI R17, RZ, 0x2, R4 ;
 
-        /*0210*/                   STG.E.64.SYS [R16+0x10], R6 ;
+        /*0210*/                   LOP3.LUT R7, R3.reuse, R2, R16, 0x96, !PT ;
 
-        /*0220*/                   IMAD.WIDE.U32 R4, R4, 0x200000, RZ ;
+        /*0220*/                   IMAD.SHL.U32 R2, R3, 0x10, RZ ;
 
-        /*0230*/                   LOP3.LUT R4, R4, R3, RZ, 0x3c, !PT ;
+        /*0230*/                   LOP3.LUT R17, R17, R4, RZ, 0x3c, !PT ;
 
-        /*0240*/                   I2F.F64.U64 R2, R4 ;
+        /*0240*/                   LOP3.LUT R4, R7, R2, RZ, 0x3c, !PT ;
 
-        /*0250*/                   DFMA R20, R2, R18, 5.5511151231257827021e-17 ;
+        /*0250*/                   IMAD.SHL.U32 R2, R17, 0x2, RZ ;
 
-        /*0260*/                   STG.E.64.SYS [R10], R20 ;
+        /*0260*/                   SHF.R.U32.HI R6, RZ, 0x2, R5 ;
 
-        /*0270*/                   LDG.E.64.SYS R2, [R16] ;
+        /*0270*/                   LOP3.LUT R17, R4.reuse, R2, R17, 0x96, !PT ;
 
-        /*0280*/                   LDG.E.64.SYS R12, [R16+0x8] ;
+        /*0280*/                   IMAD.SHL.U32 R2, R4, 0x10, RZ ;
 
-        /*0290*/                   LDG.E.64.SYS R4, [R16+0x10] ;
+        /*0290*/                   LOP3.LUT R6, R6, R5, RZ, 0x3c, !PT ;
 
-        /*02a0*/                   SHF.R.U32.HI R0, RZ, 0x2, R3 ;
+        /*02a0*/                   LOP3.LUT R5, R17, R2, RZ, 0x3c, !PT ;
 
-        /*02b0*/                   LOP3.LUT R0, R0, R3, RZ, 0x3c, !PT ;
+        /*02b0*/                   IMAD.SHL.U32 R2, R6, 0x2, RZ ;
 
-        /*02c0*/                   STG.E.SYS [R16+0x4], R13 ;
+        /*02c0*/                   SHF.R.U32.HI R16, RZ, 0x2, R11 ;
 
-        /*02d0*/                   SHF.R.U32.HI R23, RZ, 0x2, R12 ;
+        /*02d0*/                   LOP3.LUT R7, R5.reuse, R2, R6, 0x96, !PT ;
 
-        /*02e0*/                   IMAD.SHL.U32 R14, R5, 0x10, RZ ;
+        /*02e0*/                   IMAD.SHL.U32 R6, R5, 0x10, RZ ;
 
-        /*02f0*/                   STG.E.64.SYS [R16+0x8], R4 ;
+        /*02f0*/                   LOP3.LUT R17, R16, R11, RZ, 0x3c, !PT ;
 
-        /*0300*/                   IMAD.SHL.U32 R3, R0, 0x2, RZ ;
+        /*0300*/                   LOP3.LUT R6, R7, R6, RZ, 0x3c, !PT ;
 
-        /*0310*/                   LOP3.LUT R23, R23, R12, RZ, 0x3c, !PT ;
+        /*0310*/                   IMAD.SHL.U32 R2, R17, 0x2, RZ ;
 
-        /*0320*/                   IADD3 R12, R2, 0xb0f8a, RZ ;
+        /*0320*/                   IMAD.SHL.U32 R7, R6.reuse, 0x10, RZ ;
 
-        /*0330*/                   LOP3.LUT R3, R5, R3, R0, 0x96, !PT ;
+        /*0330*/                   LOP3.LUT R2, R6, R2, R17, 0x96, !PT ;
 
-        /*0340*/                   IMAD.SHL.U32 R0, R23, 0x2, RZ ;
+        /*0340*/                   IADD3 R16, R10, 0xb0f8a, R3 ;
 
-        /*0350*/                   LOP3.LUT R14, R3, R14, RZ, 0x3c, !PT ;
+        /*0350*/                   LOP3.LUT R7, R2, R7, RZ, 0x3c, !PT ;
 
-        /*0360*/                   STG.E.SYS [R16], R12 ;
+        /*0360*/                   IADD3 R2, R10, 0x212e9e, RZ ;
 
-        /*0370*/                   LOP3.LUT R0, R14.reuse, R0, R23, 0x96, !PT ;
+        /*0370*/                   IMAD.WIDE.U32 R16, R16, 0x200000, RZ ;
 
-        /*0380*/                   IMAD.SHL.U32 R15, R14, 0x10, RZ ;
+        /*0380*/                   IADD3 R22, R10, 0x161f14, R5 ;
 
-        /*0390*/                   IADD3 R23, R2, 0x587c5, R14 ;
+        /*0390*/                   IMAD.IADD R18, R2, 0x1, R7 ;
 
-        /*03a0*/                   LOP3.LUT R15, R0, R15, RZ, 0x3c, !PT ;
+        /*03a0*/                   IADD3 R11, R10, 0x587c5, R11 ;
 
-        /*03b0*/                   IMAD.IADD R6, R15, 0x1, R12 ;
+        /*03b0*/                   IMAD.WIDE.U32 R22, R22, 0x200000, RZ ;
 
-        /*03c0*/                   STG.E.64.SYS [R16+0x10], R14 ;
+        /*03c0*/                   IMAD.WIDE.U32 R18, R18, 0x200000, RZ ;
 
-        /*03d0*/                   IMAD.WIDE.U32 R6, R6, 0x200000, RZ ;
+        /*03d0*/                   LOP3.LUT R16, R16, R11, RZ, 0x3c, !PT ;
 
-        /*03e0*/                   LOP3.LUT R22, R6, R23, RZ, 0x3c, !PT ;
+        /*03e0*/                   IADD3 R11, R10.reuse, 0x1ba6d9, R6 ;
 
-        /*03f0*/                   IMAD.MOV.U32 R23, RZ, RZ, R7 ;
+        /*03f0*/                   IADD3 R25, R10, 0x10974f, R4 ;
 
-        /*0400*/                   I2F.F64.U64 R2, R22 ;
+        /*0400*/                   LOP3.LUT R18, R18, R11, RZ, 0x3c, !PT ;
 
-        /*0410*/                   DFMA R2, R2, R18, 5.5511151231257827021e-17 ;
+        /*0410*/                   IMAD.MOV.U32 R11, RZ, RZ, R23 ;
 
-        /*0420*/                   DMUL R20, R2, 5 ;
+        /*0420*/                   LOP3.LUT R10, R22, R25, RZ, 0x3c, !PT ;
 
-        /*0430*/                   STG.E.64.SYS [R10+0x8], R20 ;
+        /*0430*/                   I2F.F64.U64 R16, R16 ;
 
-        /*0440*/                   LDG.E.64.SYS R6, [R16] ;
+        /*0440*/                   I2F.F64.U64 R18, R18 ;
 
-        /*0450*/                   LDG.E.64.SYS R8, [R16+0x8] ;
+        /*0450*/                   I2F.F64.U64 R10, R10 ;
 
-        /*0460*/                   LDG.E.64.SYS R2, [R16+0x10] ;
+        /*0460*/                   IMAD.MOV.U32 R22, RZ, RZ, 0x0 ;
 
-        /*0470*/                   IMAD.MOV.U32 R14, RZ, RZ, 0x0 ;
+        /*0470*/                   IMAD.MOV.U32 R23, RZ, RZ, 0x3ca00000 ;
 
-        /*0480*/                   IMAD.MOV.U32 R15, RZ, RZ, 0x40150000 ;
+        /*0480*/                   DFMA R24, R16, R22, 5.5511151231257827021e-17 ;
 
-        /*0490*/                   SHF.R.U32.HI R0, RZ, 0x2, R7 ;
+        /*0490*/                   DFMA R26, R10, R22, 5.5511151231257827021e-17 ;
 
-        /*04a0*/                   LOP3.LUT R0, R0, R7, RZ, 0x3c, !PT ;
+        /*04a0*/                   DFMA R22, R18, R22, 5.5511151231257827021e-17 ;
 
-        /*04b0*/                   SHF.R.U32.HI R7, RZ, 0x2, R8 ;
+        /*04b0*/                   IMAD.WIDE.U32 R10, R0, R20, c[0x0][0x170] ;
 
-        /*04c0*/                   IMAD.SHL.U32 R4, R3, 0x10, RZ ;
+        /*04c0*/                   DMUL R26, R26, 5 ;
 
-        /*04d0*/                   STG.E.64.SYS [R16+0x8], R2 ;
+        /*04d0*/                   DFMA R22, R22, R28, 2.5 ;
 
-        /*04e0*/                   IMAD.SHL.U32 R5, R0, 0x2, RZ ;
+        /*04e0*/                   STG.E.64.SYS [R10], R24 ;
 
-        /*04f0*/                   LOP3.LUT R7, R7, R8, RZ, 0x3c, !PT ;
+        /*04f0*/                   STG.E.64.SYS [R10+0x8], R26 ;
 
-        /*0500*/                   IADD3 R8, R6, 0xb0f8a, RZ ;
+        /*0500*/                   STG.E.64.SYS [R10+0x10], R22 ;
 
-        /*0510*/                   LOP3.LUT R5, R3, R5, R0, 0x96, !PT ;
+        /*0510*/                   STG.E.64.SYS [R14], R2 ;
 
-        /*0520*/                   IMAD.SHL.U32 R0, R7, 0x2, RZ ;
+        /*0520*/                   STG.E.64.SYS [R14+0x8], R4 ;
 
-        /*0530*/                   LOP3.LUT R4, R5, R4, RZ, 0x3c, !PT ;
+        /*0530*/                   STG.E.64.SYS [R14+0x10], R6 ;
 
-        /*0540*/                   STG.E.64.SYS [R16], R8 ;
+        /*0540*/                   STG.E.64.SYS [R14+0x18], R8 ;
 
-        /*0550*/                   LOP3.LUT R0, R4.reuse, R0, R7, 0x96, !PT ;
+        /*0550*/                   STG.E.64.SYS [R14+0x28], R12 ;
 
-        /*0560*/                   IMAD.SHL.U32 R5, R4, 0x10, RZ ;
+        /*0560*/                   STG.E.SYS [R14+0x20], R21 ;
 
-        /*0570*/                   IADD3 R7, R6, 0x587c5, R4 ;
+        /*0570*/                   EXIT ;
 
-        /*0580*/                   LOP3.LUT R5, R0, R5, RZ, 0x3c, !PT ;
+        /*0580*/                   BRA 0x580;
 
-        /*0590*/                   IMAD.IADD R12, R5, 0x1, R8 ;
+        /*0590*/                   NOP;
 
-        /*05a0*/                   STG.E.64.SYS [R16+0x10], R4 ;
+        /*05a0*/                   NOP;
 
-        /*05b0*/                   IMAD.WIDE.U32 R12, R12, 0x200000, RZ ;
+        /*05b0*/                   NOP;
 
-        /*05c0*/                   LOP3.LUT R12, R12, R7, RZ, 0x3c, !PT ;
+        /*05c0*/                   NOP;
 
-        /*05d0*/                   I2F.F64.U64 R6, R12 ;
+        /*05d0*/                   NOP;
 
-        /*05e0*/                   DFMA R6, R6, R18, 5.5511151231257827021e-17 ;
+        /*05e0*/                   NOP;
 
-        /*05f0*/                   DFMA R6, R6, R14, 2.5 ;
-
-        /*0600*/                   STG.E.64.SYS [R10+0x10], R6 ;
-
-        /*0610*/                   EXIT ;
-
-        /*0620*/                   BRA 0x620;
-
-        /*0630*/                   NOP;
-
-        /*0640*/                   NOP;
-
-        /*0650*/                   NOP;
-
-        /*0660*/                   NOP;
-
-        /*0670*/                   NOP;
+        /*05f0*/                   NOP;
 
 		...................................................................................
 
--- celer-rng-uniform/uniform.s
+++ celer-rng-uniform/uniform.s
@@ -16,207 +16,191 @@ compile_size = 64bit
 
         /*0020*/                   S2R R0, SR_CTAID.X ;
 
-        /*0030*/                   IMAD.MOV.U32 R17, RZ, RZ, 0x30 ;
+        /*0030*/                   IMAD.MOV.U32 R15, RZ, RZ, 0x30 ;
 
         /*0040*/                   S2R R3, SR_TID.X ;
 
         /*0050*/                   IMAD R0, R0, c[0x0][0x0], R3 ;
 
-        /*0060*/                   IMAD.WIDE.U32 R16, R0, R17, c[0x0][0x160] ;
+        /*0060*/                   IMAD.WIDE.U32 R14, R0, R15, c[0x0][0x160] ;
 
-        /*0070*/                   LDG.E.64.SYS R2, [R16] ;
+        /*0070*/                   LDG.E.64.SYS R10, [R14] ;
 
-        /*0080*/                   LDG.E.64.SYS R14, [R16+0x8] ;
+        /*0080*/                   LDG.E.64.SYS R2, [R14+0x8] ;
 
-        /*0090*/                   LDG.E.64.SYS R8, [R16+0x10] ;
+        /*0090*/                   LDG.E.64.SYS R4, [R14+0x10] ;
 
-        /*00a0*/                   IMAD.MOV.U32 R18, RZ, RZ, 0x0 ;
+        /*00a0*/                   LDG.E.64.SYS R8, [R14+0x18] ;
 
-        /*00b0*/                   IMAD.MOV.U32 R19, RZ, RZ, 0x3ca00000 ;
+        /*00b0*/                   LDG.E.64.SYS R12, [R14+0x28] ;
 
-        /*00c0*/                   IMAD.MOV.U32 R11, RZ, RZ, 0x8 ;
+        /*00c0*/                   LDG.E.SYS R23, [R14+0x20] ;
 
-        /*00d0*/                   IMAD R10, R0, 0x3, RZ ;
+        /*00d0*/                   IMAD.MOV.U32 R29, RZ, RZ, 0x8 ;
 
-        /*00e0*/                   IMAD.WIDE.U32 R10, R10, R11, c[0x0][0x170] ;
+        /*00e0*/                   IMAD R0, R0, 0x3, RZ ;
 
-        /*00f0*/                   SHF.R.U32.HI R4, RZ, 0x2, R3 ;
+        /*00f0*/                   SHF.R.U32.HI R6, RZ, 0x2, R11 ;
 
-        /*0100*/                   LOP3.LUT R4, R4, R3, RZ, 0x3c, !PT ;
+        /*0100*/                   LOP3.LUT R6, R6, R11, RZ, 0x3c, !PT ;
 
-        /*0110*/                   STG.E.SYS [R16+0x4], R15 ;
+        /*0110*/                   SHF.R.U32.HI R17, RZ, 0x2, R2 ;
 
-        /*0120*/                   SHF.R.U32.HI R5, RZ, 0x2, R14 ;
+        /*0120*/                   IMAD.SHL.U32 R7, R5, 0x10, RZ ;
 
-        /*0130*/                   IMAD.SHL.U32 R6, R9, 0x10, RZ ;
+        /*0130*/                   LOP3.LUT R2, R17, R2, RZ, 0x3c, !PT ;
 
-        /*0140*/                   STG.E.64.SYS [R16+0x8], R8 ;
+        /*0140*/                   IMAD.SHL.U32 R11, R6.reuse, 0x2, RZ ;
 
-        /*0150*/                   IMAD.SHL.U32 R3, R4, 0x2, RZ ;
+        /*0150*/                   LOP3.LUT R16, R6, R7, R5, 0x96, !PT ;
 
-        /*0160*/                   LOP3.LUT R5, R5, R14, RZ, 0x3c, !PT ;
+        /*0160*/                   IMAD.SHL.U32 R6, R2, 0x2, RZ ;
 
-        /*0170*/                   IADD3 R14, R2, 0xb0f8a, RZ ;
+        /*0170*/                   LOP3.LUT R11, R16, R11, RZ, 0x3c, !PT ;
 
-        /*0180*/                   LOP3.LUT R3, R9, R3, R4, 0x96, !PT ;
+        /*0180*/                   SHF.R.U32.HI R16, RZ, 0x2, R3 ;
 
-        /*0190*/                   IMAD.SHL.U32 R4, R5, 0x2, RZ ;
+        /*0190*/                   LOP3.LUT R7, R11.reuse, R6, R2, 0x96, !PT ;
 
-        /*01a0*/                   LOP3.LUT R6, R3, R6, RZ, 0x3c, !PT ;
+        /*01a0*/                   IMAD.SHL.U32 R2, R11, 0x10, RZ ;
 
-        /*01b0*/                   STG.E.SYS [R16], R14 ;
+        /*01b0*/                   LOP3.LUT R16, R16, R3, RZ, 0x3c, !PT ;
 
-        /*01c0*/                   LOP3.LUT R4, R6.reuse, R4, R5, 0x96, !PT ;
+        /*01c0*/                   LOP3.LUT R3, R7, R2, RZ, 0x3c, !PT ;
 
-        /*01d0*/                   IMAD.SHL.U32 R7, R6, 0x10, RZ ;
+        /*01d0*/                   IMAD.SHL.U32 R2, R16, 0x2, RZ ;
 
-        /*01e0*/                   IADD3 R3, R2, 0x587c5, R6 ;
+        /*01e0*/                   SHF.R.U32.HI R17, RZ, 0x2, R4 ;
 
-        /*01f0*/                   LOP3.LUT R7, R4, R7, RZ, 0x3c, !PT ;
+        /*01f0*/                   LOP3.LUT R7, R3.reuse, R2, R16, 0x96, !PT ;
 
-        /*0200*/                   IMAD.IADD R4, R7, 0x1, R14 ;
+        /*0200*/                   IMAD.SHL.U32 R2, R3, 0x10, RZ ;
 
-        /*0210*/                   STG.E.64.SYS [R16+0x10], R6 ;
+        /*0210*/                   LOP3.LUT R17, R17, R4, RZ, 0x3c, !PT ;
 
-        /*0220*/                   IMAD.WIDE.U32 R4, R4, 0x200000, RZ ;
+        /*0220*/                   LOP3.LUT R4, R7, R2, RZ, 0x3c, !PT ;
 
-        /*0230*/                   LOP3.LUT R4, R4, R3, RZ, 0x3c, !PT ;
+        /*0230*/                   IMAD.SHL.U32 R2, R17, 0x2, RZ ;
 
-        /*0240*/                   I2F.F64.U64 R2, R4 ;
+        /*0240*/                   SHF.R.U32.HI R6, RZ, 0x2, R5 ;
 
-        /*0250*/                   DFMA R2, R2, R18, 5.5511151231257827021e-17 ;
+        /*0250*/                   LOP3.LUT R17, R4.reuse, R2, R17, 0x96, !PT ;
 
-        /*0260*/                   DADD R20, RZ, R2 ;
+        /*0260*/                   IMAD.SHL.U32 R2, R4, 0x10, RZ ;
 
-        /*0270*/                   STG.E.64.SYS [R10], R20 ;
+        /*0270*/                   LOP3.LUT R6, R6, R5, RZ, 0x3c, !PT ;
 
-        /*0280*/                   LDG.E.64.SYS R2, [R16] ;
+        /*0280*/                   LOP3.LUT R5, R17, R2, RZ, 0x3c, !PT ;
 
-        /*0290*/                   LDG.E.64.SYS R12, [R16+0x8] ;
+        /*0290*/                   IMAD.SHL.U32 R2, R6, 0x2, RZ ;
 
-        /*02a0*/                   LDG.E.64.SYS R4, [R16+0x10] ;
+        /*02a0*/                   SHF.R.U32.HI R16, RZ, 0x2, R11 ;
 
-        /*02b0*/                   SHF.R.U32.HI R0, RZ, 0x2, R3 ;
+        /*02b0*/                   LOP3.LUT R7, R5.reuse, R2, R6, 0x96, !PT ;
 
-        /*02c0*/                   LOP3.LUT R0, R0, R3, RZ, 0x3c, !PT ;
+        /*02c0*/                   IMAD.SHL.U32 R6, R5, 0x10, RZ ;
 
-        /*02d0*/                   STG.E.SYS [R16+0x4], R13 ;
+        /*02d0*/                   LOP3.LUT R17, R16, R11, RZ, 0x3c, !PT ;
 
-        /*02e0*/                   SHF.R.U32.HI R23, RZ, 0x2, R12 ;
+        /*02e0*/                   LOP3.LUT R6, R7, R6, RZ, 0x3c, !PT ;
 
-        /*02f0*/                   IMAD.SHL.U32 R14, R5, 0x10, RZ ;
+        /*02f0*/                   IMAD.SHL.U32 R2, R17, 0x2, RZ ;
 
-        /*0300*/                   STG.E.64.SYS [R16+0x8], R4 ;
+        /*0300*/                   IMAD.SHL.U32 R7, R6.reuse, 0x10, RZ ;
 
-        /*0310*/                   IMAD.SHL.U32 R3, R0, 0x2, RZ ;
+        /*0310*/                   LOP3.LUT R2, R6, R2, R17, 0x96, !PT ;
 
-        /*0320*/                   LOP3.LUT R23, R23, R12, RZ, 0x3c, !PT ;
+        /*0320*/                   IADD3 R18, R10, 0xb0f8a, R3 ;
 
-        /*0330*/                   IADD3 R12, R2, 0xb0f8a, RZ ;
+        /*0330*/                   LOP3.LUT R7, R2, R7, RZ, 0x3c, !PT ;
 
-        /*0340*/                   LOP3.LUT R3, R5, R3, R0, 0x96, !PT ;
+        /*0340*/                   IADD3 R2, R10, 0x212e9e, RZ ;
 
-        /*0350*/                   IMAD.SHL.U32 R0, R23, 0x2, RZ ;
+        /*0350*/                   IMAD.WIDE.U32 R18, R18, 0x200000, RZ ;
 
-        /*0360*/                   LOP3.LUT R14, R3, R14, RZ, 0x3c, !PT ;
+        /*0360*/                   IADD3 R20, R10.reuse, 0x161f14, R5 ;
 
-        /*0370*/                   STG.E.SYS [R16], R12 ;
+        /*0370*/                   IADD3 R11, R10, 0x587c5, R11 ;
 
-        /*0380*/                   LOP3.LUT R0, R14.reuse, R0, R23, 0x96, !PT ;
+        /*0380*/                   IMAD.IADD R16, R2, 0x1, R7 ;
 
-        /*0390*/                   IMAD.SHL.U32 R15, R14, 0x10, RZ ;
+        /*0390*/                   IMAD.WIDE.U32 R20, R20, 0x200000, RZ ;
 
-        /*03a0*/                   IADD3 R23, R2, 0x587c5, R14 ;
+        /*03a0*/                   LOP3.LUT R18, R18, R11, RZ, 0x3c, !PT ;
 
-        /*03b0*/                   LOP3.LUT R15, R0, R15, RZ, 0x3c, !PT ;
+        /*03b0*/                   IMAD.WIDE.U32 R16, R16, 0x200000, RZ ;
 
-        /*03c0*/                   IMAD.IADD R6, R15, 0x1, R12 ;
+        /*03c0*/                   IADD3 R11, R10.reuse, 0x10974f, R4 ;
 
-        /*03d0*/                   STG.E.64.SYS [R16+0x10], R14 ;
+        /*03d0*/                   IADD3 R25, R10, 0x1ba6d9, R6 ;
 
-        /*03e0*/                   IMAD.WIDE.U32 R6, R6, 0x200000, RZ ;
+        /*03e0*/                   LOP3.LUT R20, R20, R11, RZ, 0x3c, !PT ;
 
-        /*03f0*/                   LOP3.LUT R22, R6, R23, RZ, 0x3c, !PT ;
+        /*03f0*/                   LOP3.LUT R16, R16, R25, RZ, 0x3c, !PT ;
 
-        /*0400*/                   IMAD.MOV.U32 R23, RZ, RZ, R7 ;
+        /*0400*/                   I2F.F64.U64 R18, R18 ;
 
-        /*0410*/                   I2F.F64.U64 R2, R22 ;
+        /*0410*/                   I2F.F64.U64 R20, R20 ;
 
-        /*0420*/                   DFMA R2, R2, R18, 5.5511151231257827021e-17 ;
+        /*0420*/                   I2F.F64.U64 R16, R16 ;
 
-        /*0430*/                   DFMA R20, R2, 5, RZ ;
+        /*0430*/                   IMAD.MOV.U32 R10, RZ, RZ, 0x0 ;
 
-        /*0440*/                   STG.E.64.SYS [R10+0x8], R20 ;
+        /*0440*/                   IMAD.MOV.U32 R11, RZ, RZ, 0x3ca00000 ;
 
-        /*0450*/                   LDG.E.64.SYS R6, [R16] ;
+        /*0450*/                   DFMA R24, R18, R10.reuse, 5.5511151231257827021e-17 ;
 
-        /*0460*/                   LDG.E.64.SYS R8, [R16+0x8] ;
+        /*0460*/                   IMAD.MOV.U32 R18, RZ, RZ, 0x0 ;
 
-        /*0470*/                   LDG.E.64.SYS R2, [R16+0x10] ;
+        /*0470*/                   DFMA R26, R20, R10, 5.5511151231257827021e-17 ;
 
-        /*0480*/                   IMAD.MOV.U32 R14, RZ, RZ, 0x0 ;
+        /*0480*/                   IMAD.MOV.U32 R19, RZ, RZ, 0x40150000 ;
 
-        /*0490*/                   IMAD.MOV.U32 R15, RZ, RZ, 0x40150000 ;
+        /*0490*/                   DFMA R10, R16, R10, 5.5511151231257827021e-17 ;
 
-        /*04a0*/                   SHF.R.U32.HI R0, RZ, 0x2, R7 ;
+        /*04a0*/                   IMAD.WIDE.U32 R20, R0, R29, c[0x0][0x170] ;
 
-        /*04b0*/                   LOP3.LUT R0, R0, R7, RZ, 0x3c, !PT ;
+        /*04b0*/                   DADD R24, RZ, R24 ;
 
-        /*04c0*/                   SHF.R.U32.HI R7, RZ, 0x2, R8 ;
+        /*04c0*/                   DFMA R26, R26, 5, RZ ;
 
-        /*04d0*/                   IMAD.SHL.U32 R4, R3, 0x10, RZ ;
+        /*04d0*/                   DFMA R10, R10, R18, 2.5 ;
 
-        /*04e0*/                   STG.E.64.SYS [R16+0x8], R2 ;
+        /*04e0*/                   STG.E.64.SYS [R20], R24 ;
 
-        /*04f0*/                   IMAD.SHL.U32 R5, R0, 0x2, RZ ;
+        /*04f0*/                   STG.E.64.SYS [R20+0x8], R26 ;
 
-        /*0500*/                   LOP3.LUT R7, R7, R8, RZ, 0x3c, !PT ;
+        /*0500*/                   STG.E.64.SYS [R20+0x10], R10 ;
 
-        /*0510*/                   IADD3 R8, R6, 0xb0f8a, RZ ;
+        /*0510*/                   STG.E.64.SYS [R14], R2 ;
 
-        /*0520*/                   LOP3.LUT R5, R3, R5, R0, 0x96, !PT ;
+        /*0520*/                   STG.E.64.SYS [R14+0x8], R4 ;
 
-        /*0530*/                   IMAD.SHL.U32 R0, R7, 0x2, RZ ;
+        /*0530*/                   STG.E.64.SYS [R14+0x10], R6 ;
 
-        /*0540*/                   LOP3.LUT R4, R5, R4, RZ, 0x3c, !PT ;
+        /*0540*/                   STG.E.64.SYS [R14+0x18], R8 ;
 
-        /*0550*/                   STG.E.64.SYS [R16], R8 ;
+        /*0550*/                   STG.E.64.SYS [R14+0x28], R12 ;
 
-        /*0560*/                   LOP3.LUT R0, R4.reuse, R0, R7, 0x96, !PT ;
+        /*0560*/                   STG.E.SYS [R14+0x20], R23 ;
 
-        /*0570*/                   IMAD.SHL.U32 R5, R4, 0x10, RZ ;
+        /*0570*/                   EXIT ;
 
-        /*0580*/                   IADD3 R7, R6, 0x587c5, R4 ;
+        /*0580*/                   BRA 0x580;
 
-        /*0590*/                   LOP3.LUT R5, R0, R5, RZ, 0x3c, !PT ;
+        /*0590*/                   NOP;
 
-        /*05a0*/                   IMAD.IADD R12, R5, 0x1, R8 ;
+        /*05a0*/                   NOP;
 
-        /*05b0*/                   STG.E.64.SYS [R16+0x10], R4 ;
+        /*05b0*/                   NOP;
 
-        /*05c0*/                   IMAD.WIDE.U32 R12, R12, 0x200000, RZ ;
+        /*05c0*/                   NOP;
 
-        /*05d0*/                   LOP3.LUT R12, R12, R7, RZ, 0x3c, !PT ;
+        /*05d0*/                   NOP;
 
-        /*05e0*/                   I2F.F64.U64 R6, R12 ;
+        /*05e0*/                   NOP;
 
-        /*05f0*/                   DFMA R6, R6, R18, 5.5511151231257827021e-17 ;
-
-        /*0600*/                   DFMA R6, R6, R14, 2.5 ;
-
-        /*0610*/                   STG.E.64.SYS [R10+0x10], R6 ;
-
-        /*0620*/                   EXIT ;
-
-        /*0630*/                   BRA 0x630;
-
-        /*0640*/                   NOP;
-
-        /*0650*/                   NOP;
-
-        /*0660*/                   NOP;
-
-        /*0670*/                   NOP;
+        /*05f0*/                   NOP;
 
 		...................................................................................
 
